974d732e92d6d06b3d6467013bfc795889b6ba31
FelipeALuna <felipeluna_2000@hotmail.com>
2024-04-18 13:49:51 -0300

    getById

---
+++ config/Database.php
@@ -0,0 +1,25 @@
+<?php
+
+class Database
+{
+
+    private $host = 'localhost';
+    private $dbName = 'gestao_ong';
+    private $userName  = 'root';
+    private $password = '';
+    
+
+    public function getConnection()
+    {
+        try {
+            $pdo = new PDO("mysql:host=localhost", $this->userName, $this->password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dbname = "`" . str_replace("`", "``", $this->dbName) . "`";
+            $pdo->query("CREATE DATABASE IF NOT EXISTS $dbname");
+            $pdo->query("use $dbname");
+        } catch (PDOException $exeption) {
+            echo $exeption->getMessage();
+        }
+        return $pdo;
+    }
+}
---
+++ models/Product.php
@@ -0,0 +1,75 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_error',1);
+
+class Product{
+    private $id;
+    private $name;
+    private $quantity;
+    private $description;
+    private $create_at;
+    private $updated_at;
+
+
+    private $connection;
+    private $table = "products";
+
+    
+    public function __construct($database){
+        $this->connection = $database;
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "CREATE TABLE IF NOT EXISTS $this->table (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255),
+            description TEXT,
+            quantity INTEGER,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )";
+        
+        $this->connection->exec($sql);
+    }
+
+    public function getAllProducts(){
+        $query = "SELECT * FROM $this->table";
+        $productList = $this->connection->prepare($query);
+        $productList->execute();
+        return $productList;
+    }
+    public function getProductById($id){
+        $this->id = $id;
+        $query = 'SELECT * FROM '.$this->table.' WHERE id= ? LIMIT 0,1'; 
+        $product = $this->connection->prepare($query);
+        $product->execute([$this->id]);
+        return $product;
+
+    }
+    public function insertProduct($params){
+        try{
+
+            $this->name = $params["name"];
+            $this->quantity =$params["quantity"];
+            $this->description =$params["description"];
+
+            $query = 'INSERT INTO '.$this->table.' SET name=:name,quantity=:quantity,description=:description';
+
+            $product = $this->connection->prepare($query);
+
+            $product->bindValue('name', $this->name);
+            $product->bindValue('quantity',$this->quantity);
+            $product->bindValue('description', $this->description);
+
+            if($product->execute()){
+                return true;
+            }else{
+                return false;
+            }
+           
+
+        }catch(PDOException $exeption){
+            echo $exeption->getMessage();
+        }
+    }
+
+}
